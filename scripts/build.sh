cd7519273726a37e8b94cadbda758ff5ac216e09
Gagan Kumar <gagan1kumar.cs@gmail.com>
2020-01-26 09:35:57 +0530

    Updated scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,13 +2,15 @@
 set -e
 source "$(dirname $0)/common.sh"
 
-SOURCE=${1?"path to source.asm is missing: \$1"}
-SOURCE_DIR="$(dirname $SOURCE)"
-BINARY=${2?"path for output binary is missing: \$2"}
+SOURCE_DIR=${1?"path to source dir is missing: \$1"}
+BUILD_DIR=${2?"path to build dir is missing: \$2"}
 MAX_SIZE=446
 
 echo "[Build] Starting..."
-nasm -f bin "${SOURCE:?}" -o "${BINARY:?}" -i "${SOURCE_DIR}" 
-binary_size="$(stat -c '%s' ${BINARY:?})"
+nasm "${SOURCE_DIR:?}/main_low.asm" -o "${BUILD_DIR:?}/main_low.o" -f elf64
+gcc -c "${SOURCE_DIR:?}/main.c" -o "${BUILD_DIR:?}/main.o"
+nasm -f bin "${SOURCE_DIR:?}/init.asm" -o "${BUILD_DIR:?}/init" -i "${SOURCE_DIR}" 
+
+binary_size="$(stat -c '%s' ${BUILD_DIR:?}/init )"
 (( ${binary_size:?} <= ${MAX_SIZE:?})) || raise "Binary size exceeds ${MAX_SIZE:?} bytes, got ${binary_size:?}"
 echo "[Build] done. BinarySize: ${binary_size} bytes. MaxSize: ${MAX_SIZE} bytes."
--- scripts/run.sh
+++ scripts/run.sh
@@ -3,14 +3,26 @@ set -e
 
 # TODO: Fix dirs
 SCRIPTS_DIR="/home/scopeinfinity/OS/scripts"
+SOURCE_DIR="/home/scopeinfinity/OS/bootloader"
 BUILD_DIR="/home/scopeinfinity/OS/build"
 
-SOURCE="$(realpath main.asm)"
-BINARY="${BUILD_DIR:?}/main"
+BINARY="${BUILD_DIR:?}/init"
 DISK="$(realpath ../disk.vmdk)"
 
 mkdir -p ${BUILD_DIR:?}
 
-bash "${SCRIPTS_DIR}/build.sh" "${SOURCE}" "${BINARY}"
-bash "${SCRIPTS_DIR}/burn.sh" "${BINARY}" "${DISK}"
+function gethash {
+    local hash=$(md5sum "$1" 2> /dev/null | cut -d ' ' -f1 )
+    echo "$hash"
+}
+init_lm_s=$(gethash "${BINARY}")
+bash "${SCRIPTS_DIR}/build.sh" "${SOURCE_DIR}" "${BUILD_DIR}"
+init_lm_d=$(gethash "${BINARY}")
+if [[ "$1" = "--alsoburn" ]]; then
+    if [[ "$init_lm_s" != "$init_lm_d" ]]; then
+        bash "${SCRIPTS_DIR}/burn.sh" "${BINARY}" "${DISK}"
+    else
+        echo "Binary unchanged, ignoring burn."
+    fi
+fi
 echo "Run Completed!"
