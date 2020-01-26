fd3a2c24eac6b98e3797688658caf52065ea0da2
Gagan Kumar <gagan1kumar.cs@gmail.com>
2020-01-26 09:36:41 +0530

    Trying to do something

---
+++ bootloader/main.c
@@ -0,0 +1,4 @@
+extern int _low_print();
+void entry_point() {
+    int status = _low_print();
+}
---
+++ bootloader/main_low.asm
@@ -0,0 +1,34 @@
+%include "constants.asm"
+;;%include "io.asm___
+"
+%macro  print_string 4
+        ; Args: (str, len, x, y)
+        ; check es, bx
+        mov ax, 0x1301              ; (print string, update cursor)
+        mov bp, %1                  ; (es:bp string pointer)
+        mov cx, %2                  ; (string length)
+        mov dx, 0x%4%3              ; (pos_y, pos_x)
+        int 0x10
+%endmacro
+
+%macro  print_string_ext 7
+        ; Args: (str, len, x, y, fg_color, bg_color, page)
+        ; check es
+        mov bx, 0x%7%6%5            ; (pagenumber, attribute)
+        print_string %1, %2, %3, %4
+%endmacro
+
+global _low_print
+[SECTION .text]
+
+    _low_print:
+        push rbp
+        mov rbp, rsp
+        print_string_ext bl_welcome, bl_welcome_len, 02, 02, C_BLACK, C_WHITE, 0
+        mov rax, 15
+        leave
+        ret
+
+[SECTION .data]
+    bl_welcome             db      "TESTTT"
+    bl_welcome_len         equ       ($-bl_welcome)
