6e63f0135468d2a075b373e29fa67ba3886b38f3
Gagan Kumar <gagan1kumar.cs@gmail.com>
2020-12-11 19:16:47 +0530

    Added Bootloader Stage 2 Tests

---
+++ tests/bootloader_stage2_test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+TEST_INJECT_WORD="__TEST_INJECT_BT2__"
+TEST_MAGIC_WANT="198A65C3"
+
+source tests/lib.sh
+
+os_test_up "${TEST_INJECT_WORD:?}" || exit -1
+
+# Test
+set -e
+echo "${SCREEN_CONTENT:?}" | grep "Bootloader: Stage 2" || ( echo "Test Failed!" && exit -1 )
+echo "${SCREEN_CONTENT:?}" | grep "Loading GDT Table and entering protected mode" || ( echo "Test Failed!" && exit -1 )
+wait ${QEMU_PID:?}
+echo "$0 passed!!!"
--- tests/run.sh
+++ tests/run.sh
@@ -2,7 +2,8 @@
 set -e
 
 # Execute Tests
-bash tests/stage1_test.sh
+bash tests/bootloader_stage1_test.sh
+bash tests/bootloader_stage2_test.sh
 
 # Done
 echo "All tests passed!"
