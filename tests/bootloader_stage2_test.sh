63ca146915b6a674322675c52dd984b9e0804221
Gagan Kumar <gagan1kumar.cs@gmail.com>
2021-03-07 17:22:10 +0000

    Add hardcoded memory added in bootloader_stage2_test.sh

--- tests/bootloader_stage2_test.sh
+++ tests/bootloader_stage2_test.sh
@@ -11,8 +11,8 @@ os_test_up "${TEST_MAGIC_WANT:?}" "${TEST_INJECT_WORD:?}" || exit -1
 set -e
 
 test_screen_content $LINENO "Bootloader: Stage 2"
-test_screen_content $LINENO "Static library loaded at.*: `build_8hexbyte real_mode/static_library`"
-test_screen_content $LINENO "Kernel loaded at.*: `build_8hexbyte kernel/core`"
+test_screen_content $LINENO "Static library loaded at 0x00007E00: `build_8hexbyte real_mode/static_library`"
+test_screen_content $LINENO "Kernel loaded at 0x0000C000: `build_8hexbyte kernel/core`"
 test_screen_content $LINENO "Loading GDT Table and entering protected mode"
 
 wait ${QEMU_PID:?}
