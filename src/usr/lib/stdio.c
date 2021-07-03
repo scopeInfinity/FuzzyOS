12fd67ad274b7eb7be2e8eca63015fc1581aaa26
Gagan Kumar <gagan1kumar.cs@gmail.com>
2021-07-03 23:03:26 +0100

    Decoupled calc app from FuzzyOS; the app is still broken because of existing issue

--- src/usr/lib/stdio.c
+++ src/usr/lib/stdio.c
@@ -1,14 +1,27 @@
 #include <stdio.h>
+#include <stddef.h>
+#include <conio.h>
 #include <lib/utils/output.h>
-
-int puts (const char *s) {
-    print_line(s);
-    // TODO: Fix puts return value.
-    return 0;
-}
 
 int putchar(int c) {
     print_char(c);
     // TODO: Fix putchar return value.
     return 0;
+}
+
+int puts(const char *s) {
+    print_line(s);
+    // TODO: Fix puts return value.
+    return 0;
+}
+
+char* gets(char *s) {
+    while (1) {
+        char c = getch();
+        if (c == '\0') return NULL;
+        if (c == '\n') return s;
+        putchar(c);
+        (*s) = c;
+        s++;
+    }
 }
