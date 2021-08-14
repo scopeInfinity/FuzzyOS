70a8efd053edcf5cc1ecee608f4e99775c98e52b
Gagan Kumar <gagan1kumar.cs@gmail.com>
2021-08-14 16:36:09 +0100

    Define INIT_APPNAME at compile-time and let tests use it

--- external/src/Makefile.mk
+++ external/src/Makefile.mk
@@ -8,7 +8,9 @@ $(BUILD_DIR)/external/bin/mbr_builder: external/src/mbr_builder.c
 	mkdir -p $(dir $@)
 	$(HOST_CC) -o $@ $<
 
-$(BUILD_DIR)/external/out/fuzzy_mount: $(patsubst $(SRC_APP)/%.c,$(BUILD_APP)/%.out,$(shell find $(SRC_APP)/ -name '*.c'))
+$(BUILD_DIR)/external/out/fuzzy_mount: $(patsubst $(SRC_APP)/%.c,$(BUILD_APP)/%.out,$(shell find $(SRC_APP)/ -name '*.c')) \
+		README.md \
+		.gdbinit
     # not a mount
 	mkdir -p $@
 	cp -f -t $@ $^
