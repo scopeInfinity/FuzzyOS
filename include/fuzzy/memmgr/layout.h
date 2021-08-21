ad7ae614c32d64bd1a287c64507a3fb57e607ce1
Gagan Kumar <gagan1kumar.cs@gmail.com>
2021-08-21 17:14:37 +0100

    Increase app size to 256KB and move stack init offset

--- include/fuzzy/memmgr/layout.h
+++ include/fuzzy/memmgr/layout.h
@@ -1,11 +1,14 @@
 #pragma once
 
+// START_ENSURE_SAME_layout_asm
+#define MEMORY_APP_SIZE         0x20000
+// END_ENSURE_SAME_layout_asm
+
 // Most of the memory layout relies on the fact the kernel is first app
 // and kernel's pid is 0.
 #define PID_KERNEL 0
 
 #define MEMORY_APPBASE_LOCATION 0x10000
-#define MEMORY_APP_SIZE         0x20000
 
 #define memmgr_app_abs_location(pid) (MEMORY_APPBASE_LOCATION + (pid)*MEMORY_APP_SIZE)
 #define memmgr_app_size(pid) (MEMORY_APP_SIZE)
