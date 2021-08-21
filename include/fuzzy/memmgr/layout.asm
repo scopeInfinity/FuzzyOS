ad4c7ef2f6e4a22c9429a39e1cd5201bc3fa57a6
Gagan Kumar <gagan1kumar.cs@gmail.com>
2021-08-21 17:14:37 +0100

    Increase app size to 256KB and move stack init offset

--- include/fuzzy/memmgr/layout.asm
+++ include/fuzzy/memmgr/layout.asm
@@ -1,9 +1,17 @@
 ; constants only
 
+; START_ENSURE_SAME_layout_h
+MEMORY_APP_SIZE EQU 0x20000
+
+; END_ENSURE_SAME_layout_h
+
 ; Kernel core is app with pid 0
 ; Kernel event is trigged via IRQ0 or syscall,
+STACKINIT_APP EQU (MEMORY_APP_SIZE-4)
+STACKINIT_KERNEL_CORE EQU (STACKINIT_APP-4)
 
-STACKINIT_APP EQU 0xFFF0
-STACKINIT_KERNEL_CORE EQU STACKINIT_APP
+; Should be able to store
+; - syscall and all internal calls
+; - ISRs and all internal calls
 STACKINIT_KERNEL_EVENT EQU 0xC000
 
