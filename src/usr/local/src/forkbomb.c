2c32b06d29b5c3ba72f2bf54b10ce7ea30dbc6c4
Gagan Kumar <gagan1kumar.cs@gmail.com>
2021-09-04 22:59:51 +0100

    Add forkbomb user application

---
+++ src/usr/local/src/forkbomb.c
@@ -0,0 +1,17 @@
+// multiprocessing example
+#include <stdio.h>
+#include <process.h>
+
+
+int main(int argc,char *argv[]) {
+    while(1) {
+        int potential_parent_pid = getpid();
+        int pid = fork();
+        if(pid<0) {
+            printf("fork bomb pid: %d says it can't fork anymore\n", potential_parent_pid);
+        } else if(pid>0) {
+            printf("new fork bomb pid:%d says thanks to pid:%d\n", pid, potential_parent_pid);
+        }
+    }
+    return 0;
+}
