95997675984574e18d8e323301bb26c49f07c114
Gagan Kumar <gagan1kumar.cs@gmail.com>
2021-09-05 04:26:36 +0100

    Correctly build FuzzyOS.vmdk and FuzzyOS.vdi

--- emulator/Makefile.mk
+++ emulator/Makefile.mk
@@ -4,16 +4,16 @@ QEMU_GDB_PORT=9000
 QEMU_MONITOR_PORT=55555
 GDB_EX=echo Use GDB_EX="<command>" to execute command on connect.\n
 
-qemu: $(image_vmdk)
+qemu: $(image_raw)
 	qemu-system-x86_64 -smp 1 -m 128M -hda $< $(QEMU_SHUT_FLAGS) $(QEMU_EXTRA_FLAGS)
 
-qemu_vvv: $(image_vmdk)
+qemu_vvv: $(image_raw)
 	qemu-system-x86_64 -smp 1 -m 128M -hda $< $(QEMU_SHUT_FLAGS) $(QEMU_EXTRA_FLAGS) -d  cpu,exec,in_asm
 
-qemu_monitor: $(image_vmdk)
+qemu_monitor: $(image_raw)
 	qemu-system-x86_64 -smp 1 -m 128M -hda $< $(QEMU_SHUT_FLAGS) $(QEMU_EXTRA_FLAGS) -monitor telnet:127.0.0.1:$(QEMU_MONITOR_PORT),server,nowait
 
-qemu_debug: $(image_vmdk)
+qemu_debug: $(image_raw)
 	qemu-system-x86_64 -S -gdb tcp::$(QEMU_GDB_PORT) -smp 1 -m 128M -hda $< $(QEMU_SHUT_FLAGS) -d  cpu,exec,in_asm
 
 qemu_debug_connect:
