8f7604acfbcc8cb08f49b4ba2a740e77c5feb2ba
Gagan Kumar <gagan1kumar.cs@gmail.com>
2021-09-08 21:23:33 +0100

    Blank white screen works in VGA graphics

--- include/fuzzy/drivers/display/vga/graphics.h
+++ include/fuzzy/drivers/display/vga/graphics.h
@@ -2,5 +2,7 @@
 
 #include <stddef.h>
 
-int graphics_switchto_320x200x256();
+int graphics_set_mode(uint8_t video_mode);
+uint8_t graphics_get_mode();
+
 int graphics_write_320x200x256(int user_ds, uint8_t *__us_buffer);
--- src/kernel/syscall/graphics.c
+++ src/kernel/syscall/graphics.c
@@ -1,18 +1,38 @@
 #include <fuzzy/kernel/syscall/graphics.h>
 #include <fuzzy/drivers/display/vga/graphics.h>
-
+#include <fuzzy/memmgr/tables/gdt.h>
 #include <graphics.h>
 
+static int _graphics_prev_mode = -1;
+
+
 static int _graphics_init() {
-    // switch to 320x200x256 mode
-    return graphics_switchto_320x200x256();
+    if(_graphics_prev_mode!=-1) {
+        // already in graphics mode
+        return -1;
+    }
+    _graphics_prev_mode = graphics_get_mode();
+    int err = graphics_set_mode(GRAPHIC_MODE_320x200x16);
+    return err;
 }
 
 static int _graphics_close() {
-    return 0;
+    if(_graphics_prev_mode==-1) {
+        // not in graphics mode
+        return -1;
+    }
+    int err = graphics_set_mode(_graphics_prev_mode);
+    // flush text screen
+    flush_screen();
+    _graphics_prev_mode = -1;
+    return err;
 }
 
 static int _graphics_copybuffer(int user_ds, void *_us_buffer) {
+    uint8_t ARR[10][10];
+    for(int i=0;i<10;i++)for(int j=0;j<10;j++) {
+        ARR[i][j] = WHITE;
+    }
     return graphics_write_320x200x256(user_ds, _us_buffer);
 }
 
