6c579580c35ffe14fef30b164ee9cfc5bb2ae2ee
Gagan Kumar <gagan1kumar.cs@gmail.com>
2021-09-18 14:29:54 +0100

    Add more user program

--- src/usr/local/src/more.c
+++ src/usr/local/src/more.c
@@ -1,6 +1,10 @@
-// print content of file
+// buffered print content of file
 #include <stdio.h>
 #include <string.h>
+
+// assumption
+#define SCREEN_WIDTH  0x50
+#define SCREEN_HEIGHT 0x19
 
 void process(char filename[]) {
     FILE *handler = fopen(filename, "r");
@@ -8,7 +12,8 @@ void process(char filename[]) {
         printf("failed to open '%s' file.\n", filename);
         return;
     }
-    char buffer[80];
+    char buffer[SCREEN_WIDTH+1];
+    int count_before_buffer = SCREEN_HEIGHT-2;
     while(1) {
         if (!fgets(buffer, sizeof(buffer), handler)) {
             int err = ferror(handler);
@@ -19,12 +24,25 @@ void process(char filename[]) {
             break;
         }
         puts(buffer);
+        if(count_before_buffer>0) {
+            count_before_buffer--;
+        } else {
+            // buffer
+            char c = getch();
+            if(c=='q' || c=='Q') {
+                exit(0);
+            }
+            // continue
+        }
     }
     fclose(handler);
 }
 
 int print_usage() {
-    printf("Usage: cat <filename>\n");
+    printf("Usage: more <filename>\n");
+    printf(" > buffer will start when output takes over the whole screen\n");
+    printf(" > press 'q' to exit\n");
+    printf(" > press any other key to scroll down\n");
     return 0;
 }
 
