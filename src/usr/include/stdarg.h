c48f6746fac4af0218928cc2e25eb671230dfd59
dreampearl <rakhi.cs61@gmail.com>
2021-10-17 17:44:49 +0530

    Fix #32: va_arg for generic data types

--- src/usr/include/stdarg.h
+++ src/usr/include/stdarg.h
@@ -6,12 +6,13 @@ namespace std {
 #endif
 
 typedef struct va_list {
-    int *base;  // using int* to jmp at 4 bytes gap
+    void *base;
 } va_list;
 
-#define va_start(list, last) ((list).base = (((int*)&(last))+1))
+#define va_start(list, last) ((list).base = ((void*)(&(last)+1)))
 #define va_end(list) (list).base = NULL
-#define va_arg(list, type) (*(type*)((list).base++))
+#define va_arg(list, type) (*(type*)((list).base+=sizeof(type), \ 
+                                     (list).base-sizeof(type)))
 #define va_copy(dst, src) (dst).base = (src).base
 
 #ifdef __cplusplus
