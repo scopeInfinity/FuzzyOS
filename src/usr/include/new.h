f087ae21d0f2f4ad2d8bc1cb5176dc0769dff5ee
Gagan Kumar <gagan1kumar.cs@gmail.com>
2021-10-24 12:35:55 +0200

    [CPP] Rewrite vector<int> to work with primitive and non-primitive data-type

--- src/usr/include/new.h
+++ src/usr/include/new.h
@@ -6,3 +6,5 @@ void *operator new(std::size_t size);
 void *operator new[](std::size_t size);
 void operator delete(void *ptr);
 void operator delete[](void *ptr);
+
+void *operator new(std::size_t size, void *);
--- src/usr/include/vector.h
+++ src/usr/include/vector.h
@@ -7,28 +7,34 @@ namespace std {
 template <typename T> class vector {
     std::size_t _capacity, _size;
     T *_data;
-    void resize_capacity(std::size_t capacity);
 
   public:
     typedef T *iterator;
     typedef const T *const_iterator;
 
     vector();
+    vector(std::size_t size);
     vector(std::size_t size, const T &_default);
     vector(const vector<T> &o);
-    vector &operator=(const vector<T> &o);
     ~vector();
+
+    vector &operator=(const vector<T> &o);
+
+    void reserve(std::size_t n);
+    void resize(std::size_t new_size);
+    void resize(std::size_t new_size, const T &_default);
+    void push_back(const T &val);
+    void pop_back();
+
+    void clear();
 
     iterator begin();
     iterator end();
     const_iterator begin() const;
     const_iterator end() const;
 
-    void clear();
     bool empty() const;
     std::size_t size() const;
-    void push_back(const T &val);
-    void pop_back();
 
     T &back();
     T &front();
@@ -38,5 +44,21 @@ template <typename T> class vector {
 };
 
 } // namespace std
+
+template <typename T>
+inline bool operator!=(const std::vector<T> &a, const std::vector<T> b) {
+    if (a.size() != b.size())
+        return true;
+    for (std::size_t i = 0; i < a.size(); i++) {
+        if (a[i] != b[i])
+            return true;
+    }
+    return false;
+}
+
+template <typename T>
+inline bool operator==(const std::vector<T> &a, const std::vector<T> b) {
+    return !(a != b);
+}
 
 #include <vector.tcc>
